a21d2cc2c5aad587c06c7952bd59bbbaf75ba062
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-06-21 13:14:54 +0800

    change icon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+  <v-layout class="overflow-visible" style="height: 56px;">
+    <v-bottom-navigation v-model="value" color="teal" grow>
+      <v-btn>
+        <v-icon>mdi-home</v-icon>
+
+        Recents
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-apps</v-icon>
+
+        Favorites
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-phone</v-icon>
+
+        Nearby
+      </v-btn>
+    </v-bottom-navigation>
+  </v-layout>
 </template>
 
 <script setup>
-  //
+//
 </script>
