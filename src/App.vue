70e5e244c91614e7f2ffe2332c69cfa5bc9bbb6d
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-06-21 13:18:43 +0800

    alter icon name

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,21 @@
   <v-layout class="overflow-visible" style="height: 56px;">
     <v-bottom-navigation v-model="value" color="teal" grow>
       <v-btn>
-        <v-icon>mdi-home</v-icon>
+        <v-icon>mdi-account</v-icon>
 
-        Recents
+        Profile
       </v-btn>
 
       <v-btn>
         <v-icon>mdi-apps</v-icon>
 
-        Favorites
+        Projects
       </v-btn>
 
       <v-btn>
         <v-icon>mdi-phone</v-icon>
 
-        Nearby
+        Contact
       </v-btn>
     </v-bottom-navigation>
   </v-layout>
