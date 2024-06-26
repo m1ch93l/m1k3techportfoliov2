ce9b59e0b809c51d1f794512a94c3b9fc6626e4a
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-06-26 12:07:05 +0800

    alter tabs

--- src/components/TabsNavigation.vue
+++ src/components/TabsNavigation.vue
@@ -1,40 +1,49 @@
 <template>
   <v-sheet color="#0d1117" elevation="3" rounded="lg">
-    <v-tabs v-model="tab" :items="tabs" align-tabs="center" color="white" height="60" slider-color="#f78166">
-      <template v-slot:tab="{ item }">
-        <v-tab :prepend-icon="item.icon" :text="item.text" :value="item.value" class="text-none"></v-tab>
-      </template>
-
-      <template v-slot:item="{ item }">
-        <YourPageComponent v-if="item.value === 'tab-1'" />
-        <AnotherPageComponent v-if="item.value === 'tab-2'" />
-        <YourThirdPageComponent v-if="item.value === 'tab-3'" />
+    <v-tabs v-model="tab" align-tabs="center" color="white" height="60" slider-color="#f78166">
+      <template v-for="item in tabs" :key="item.route">
+        <router-link :to="item.route" class="v-tab text-none" :class="{ 'v-tab--active': tab === item.route }"
+          @click="tab = item.route">
+          <v-tab :prepend-icon="item.icon">{{ item.text }}</v-tab>
+        </router-link>
       </template>
     </v-tabs>
+    <v-tabs-window class="pa-4">
+      <router-view />
+    </v-tabs-window>
   </v-sheet>
 </template>
 
-<script>
-export default {
-  data: () => ({
-    tab: 'tab-1',
-    tabs: [
-      {
-        icon: 'mdi-book-open-page-variant',
-        text: 'Readme',
-        value: 'tab-1',
-      },
-      {
-        icon: 'mdi-handshake-outline',
-        text: 'Code of Conduct',
-        value: 'tab-2',
-      },
-      {
-        icon: 'mdi-license',
-        text: 'MIT License',
-        value: 'tab-3',
-      },
-    ],
-  }),
-}
+<script setup>
+import { ref, watch } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
+
+const tab = ref('/index');
+
+const tabs = [
+  {
+    icon: 'mdi-book-open-page-variant',
+    text: 'Readme',
+    route: '/',
+  },
+  {
+    icon: 'mdi-handshake-outline',
+    text: 'Code of Conduct',
+    route: '/another',
+  },
+  {
+    icon: 'mdi-license',
+    text: 'MIT License',
+    route: '/tab-3',
+  },
+];
+
+const route = useRoute();
+watch(
+  () => route.path,
+  (newPath) => {
+    tab.value = newPath;
+  },
+  { immediate: true }
+);
 </script>
