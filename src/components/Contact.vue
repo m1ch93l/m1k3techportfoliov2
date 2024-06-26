06614a247f4dfe04c3f3ed99726363f8551a15e0
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-06-27 00:13:34 +0800

    initial contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,3 +1,10 @@
 <template>
-  <div class="text-h1">Contact Page</div>
+  <v-container class="fill-height" style="min-height: 650px" fluid>
+    <v-row>
+      <v-col class="text-center" cols="12">
+        <h2 class="text-h3 text-uppercase font-weight-bold">Contact me</h2>
+        <p class="text-body-1">Feel free to text or call me to say hi!</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,10 +2,10 @@
   <!-- Profile -->
   <v-container class="fill-height" style="min-height: 650px" fluid>
     <v-row>
-      <v-col class="text-center" cols="6">
+      <v-col class="text-center" cols-lg="6" cols-sm="12">
         <v-avatar image="https://avatars.githubusercontent.com/u/91449532?v=4" size="450"></v-avatar>
       </v-col>
-      <v-col class="text-center align-content-center" cols="6">
+      <v-col class="text-center align-content-center" cols-lg="6" cols-sm="12">
         <div class="text-h4 text-uppercase text-medium-emphasis">hello, coders, i'm</div>
         <div class="text-h2 text-uppercase font-weight-bold">michael b. bañaria</div>
         <div class="text-center text-uppercase text-medium-emphasis"
