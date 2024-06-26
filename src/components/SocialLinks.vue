9dcfb5ece060dbcee148b84fead9d3e600be59cb
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-06-26 10:51:42 +0800

    social links

---
+++ src/components/SocialLinks.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="mt-5">
+    <v-btn icon href="https://facebook.com">
+      <v-icon size="24">mdi-facebook</v-icon>
+    </v-btn>
+    <v-btn icon href="https://twitter.com">
+      <v-icon size="24">mdi-twitter</v-icon>
+    </v-btn>
+    <v-btn icon href="https://linkedin.com">
+      <v-icon size="24">mdi-linkedin</v-icon>
+    </v-btn>
+    <v-btn icon href="https://github.com">
+      <v-icon size="24">mdi-github</v-icon>
+    </v-btn>
+  </div>
+</template>
