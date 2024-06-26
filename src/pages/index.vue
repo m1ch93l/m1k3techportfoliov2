3ffa00159ca65f37253e816f31c09c9c772f134d
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-06-26 10:08:35 +0800

    add avatar

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,14 @@
 <template>
-  
+  <!-- Profile -->
+  <v-container class="fill-height" style="min-height: 650px" fluid>
+    <v-row max-height>
+      <v-col class="text-center" cols="6">
+        <v-avatar image="https://avatars.githubusercontent.com/u/91449532?v=4" size="450"></v-avatar>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-  //
+//
 </script>
