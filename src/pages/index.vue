c945510c37ebc6e6e73c7dc7f46094a237f296fc
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-06-26 10:31:16 +0800

    right side fullname

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,13 @@
 <template>
   <!-- Profile -->
   <v-container class="fill-height" style="min-height: 650px" fluid>
-    <v-row max-height>
+    <v-row>
       <v-col class="text-center" cols="6">
         <v-avatar image="https://avatars.githubusercontent.com/u/91449532?v=4" size="450"></v-avatar>
+      </v-col>
+      <v-col class="text-center align-content-center" cols="6">
+        <div class="text-h4 text-uppercase text-medium-emphasis">hello, coders, i'm</div>
+        <div class="text-h2 text-uppercase font-weight-bold">michael b. bañaria</div>
       </v-col>
     </v-row>
   </v-container>
