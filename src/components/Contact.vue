5ef7f94aba1603ffc47422fcabd8cd4588b5efbc
m1ch93l <mbbmichaelbolobanaria25@gmail.com>
2024-06-27 18:28:47 +0800

    add social link to contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,6 +5,10 @@
         <h2 class="text-h3 text-uppercase font-weight-bold">Contact me</h2>
         <p class="text-body-1">Feel free to text or call me to say hi!</p>
       </v-col>
+
+      <v-col class="text-center" cols="12">
+        <SocialLinks />
+      </v-col>
     </v-row>
   </v-container>
 </template>
